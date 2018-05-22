d17ea9422790ce1ac650957c7dcffbd431359e56
Christian Jess Mark <christianjessmark@gmail.com>
2018-05-22 14:51:57 +0200

    Added reset and change score functionality

--- scorekeeper.js
+++ scorekeeper.js
@@ -3,6 +3,8 @@ let p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1display");
 var p2Display = document.querySelector("#p2display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -18,6 +20,7 @@ p1Button.addEventListener("click",function(){
 		p1Display.textContent = p1Score
 	}
 });
+
 p2Button.addEventListener("click",function(){
 	if (!gameOver) {
 		p2Score++
@@ -27,4 +30,24 @@ p2Button.addEventListener("click",function(){
 		}
 		p2Display.textContent = p2Score
 	}
+});
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
 });
